cb07e28c34f7e106f581b2eae7b95ee866a989b1
Snuggie <snugglefuzz@hotmail.com>
2024-03-31 15:33:27 -0700

    Seperated Riot fetching logic to its own py file.

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
 from database import create_connection
+from riot_api import get_riot_account_info, get_summoner_info
 from config_loader import API_KEY
 import httpx
 
@@ -12,25 +13,6 @@ def get_db():
     finally:
         db.close()
 
-async def get_riot_account_info(name: str, tag: str):
-    url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}"
-    headers = {"X-Riot-Token": API_KEY}
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        raise HTTPException(status_code=response.status_code, detail="Failed to fetch player data.")
-
-async def get_summoner_info(puuid: str):
-    url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
-    headers = {"X-Riot-Token": API_KEY}
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, headers=headers)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        response.raise_for_status()
 
 @app.get('/player/')
 async def get_player(name: str = Query(...), tag: str = Query(...)):
---
+++ FastAPI/riot_api.py
@@ -0,0 +1,20 @@
+import httpx
+from config_loader import API_KEY
+
+async def fetch_from_riot_api(url: str):
+    headers = {"X-Riot-Token": API_KEY}
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        # Raise an exception for HTTP error responses
+        response.raise_for_status()
+
+async def get_riot_account_info(name: str, tag: str):
+    url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}"
+    return await fetch_from_riot_api(url)
+
+async def get_summoner_info(puuid: str):
+    url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
+    return await fetch_from_riot_api(url)

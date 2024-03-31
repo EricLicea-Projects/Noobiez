94a33161ef52b070ef2ed74f8a0685d42a92e916
Snuggie <snugglefuzz@hotmail.com>
2024-03-31 16:17:33 -0700

    Player data is saving correctly in database

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -1,7 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
-DATABASE = '/database/noobiez.db'
+DATABASE = 'database/noobiez.db'
 
 def create_connection():
     conn = None
@@ -12,3 +12,28 @@ def create_connection():
         print(e)
     
     return conn
+
+
+def upsert_player_data(player_data):
+    sql = '''
+    INSERT INTO players(puuid, accountId, gameName, tagLine, profileIconId, summonerLevel)
+    VALUES(?,?,?,?,?,?)
+    ON CONFLICT(puuid) DO UPDATE SET
+    accountId=excluded.accountId,
+    gameName=excluded.gameName,
+    tagLine=excluded.tagLine,
+    profileIconId=excluded.profileIconId,
+    summonerLevel=excluded.summonerLevel;
+    '''
+
+    conn = create_connection()
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, (player_data['puuid'], player_data['accountId'], player_data['gameName'],
+                          player_data['tagLine'], player_data['profileIconId'], player_data['summonerLevel']))
+        conn.commit()
+    except Error as e:
+        print(f"Database error:{e}")
+    finally:
+        if conn:
+            conn.close()
--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
-from database import create_connection
+from database import create_connection, upsert_player_data
 from riot_api import get_riot_account_info, get_summoner_info
-from config_loader import API_KEY
-import httpx
+
 
 app = FastAPI()
 
@@ -24,12 +23,14 @@ async def get_player(name: str = Query(...), tag: str = Query(...)):
         summoner_info = await get_summoner_info(puuid)
 
         player_data.update({
-            'accountID': summoner_info.get('accountId'),
+            'accountId': summoner_info.get('accountId'),
             'profileIconId': summoner_info.get('profileIconId'),
             'summonerLevel': summoner_info.get('summonerLevel')
         })
     else:
         raise HTTPException(status_code=404, detail='Player no Found')
+    
+    upsert_player_data(player_data)
     
     return player_data
 

c59751cb3f7ec53ea932d5dc2e13a6260a54fb6e
Snuggie <snugglefuzz@hotmail.com>
2024-04-08 14:09:30 -0700

    New API to get list of player MAtch Id's

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from database import create_connection, upsert_player_data, get_player_info
-from riot_api import get_riot_account_info, get_summoner_info
+from riot_api import get_riot_account_info, get_summoner_info, get_match_ids
 
 
 app = FastAPI()
@@ -46,9 +46,17 @@ async def get_player(name: str = Query(...), tag: str = Query(...)):
 
 
 @app.get("/players/")
-async def read_player(gameName: str, tagLine: str):
+async def read_player(gameName: str = Query(...), tagLine: str = Query(...)):
     player_info = get_player_info(gameName, tagLine)
     if player_info:
         return player_info
     else:
         raise HTTPException(status_code=404, detail="Player not found")
+    
+@app.get('/matches/')
+async def get_matches(puuid: str = Query(...)):
+    match_ids = await get_match_ids(puuid)
+    if match_ids:
+        return match_ids
+    else:
+        raise HTTPException(status_code=400, detail="Player not Found")
--- FastAPI/riot_api.py
+++ FastAPI/riot_api.py
@@ -8,13 +8,19 @@ async def fetch_from_riot_api(url: str):
     if response.status_code == 200:
         return response.json()
     else:
-        # Raise an exception for HTTP error responses
         response.raise_for_status()
+
 
 async def get_riot_account_info(name: str, tag: str):
     url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}"
     return await fetch_from_riot_api(url)
 
+
 async def get_summoner_info(puuid: str):
     url = f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
+    return await fetch_from_riot_api(url)
+
+
+async def get_match_ids(puuid: str) -> list:
+    url = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
     return await fetch_from_riot_api(url)

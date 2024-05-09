ea6b6261d3c27b577592231dc14d427a46c785a7
Snuggie <snugglefuzz@hotmail.com>
2024-05-09 14:18:59 -0700

    Forcing Dark Mode

--- React/Noobiez/src/main.tsx
+++ React/Noobiez/src/main.tsx
@@ -4,11 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./themes/theme";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
